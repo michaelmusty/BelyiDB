cd182c5195bb3ffee56fc608c930bc8fd0a20944
michaelmusty <michaelmusty@gmail.com>
2018-05-24 16:44:40 -0400

    update usage.m

--- usage.m
+++ usage.m
@@ -1,3 +1,17 @@
-// load this file to see some basic examples of how to use BelyiDB
+/* load this file to see some basic examples of how to use BelyiDB */
 
+// attach BelyiDB intrinsics
 AttachSpec("code/spec");
+
+// get filenames for all degree 6, genus 1 passports
+filenames := BelyiDBGetFilenames(6,1);
+
+// get BelyiDB objects for all degree 6, genus 1 passports (only hyperbolic this time)
+objs := BelyiDBGet(6,1 : only_hyperbolic := true);
+
+// 6T15-[5,5,5]-51-51-51-g1
+s := BelyiDBRead("6T15-[5,5,5]-51-51-51-g1.m");
+base_field_data := BaseFieldData(s);
+curves := BelyiCurves(s);
+maps := BelyiMaps(s);
+galois_orbits := GaloisOrbits(s);
