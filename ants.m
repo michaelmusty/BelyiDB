7217a8abf07c45e0a6a7860416cef4ed1f8a4c46
Sam Schiavone <sam.schiavone@gmail.com>
2018-07-15 21:34:18 -0500

    changed example

--- ants.m
+++ ants.m
@@ -1,5 +1,8 @@
 load "config.m";
-s := BelyiDBRead("8T48-[7,3,4]-71-3311-44-g1.m");
+//s := BelyiDBRead("8T48-[7,3,4]-71-3311-44-g1.m");
+s := BelyiDBRead("6T15-[5,5,5]-51-51-51-g1.m");
 Gammas := BelyiDBToGammas(s);
-time X, phi := BelyiMap(Gammas[8] : prec := 45, precNewton := 1000);
+Gamma := Gammas[1];
+prec := 30;
+time X,phi := BelyiMap(Gamma : prec := prec, precNewton := 800);
 K<nu> := BaseField(X);
