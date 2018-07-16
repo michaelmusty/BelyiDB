55fd4f5ea305e5d725065e12b11116ba86fd0b83
michaelmusty <michaelmusty@gmail.com>
2018-07-15 21:34:59 -0500

    try to merge

--- ants.m
+++ ants.m
@@ -1,5 +1,12 @@
+/* SETTINGS */
+
+// 50 attempts @ prec=35, precNewton=300
+// 28 success
+// 18 blows up
+// 4 Newton fails
+
 load "config.m";
 s := BelyiDBRead("8T48-[7,3,4]-71-3311-44-g1.m");
 Gammas := BelyiDBToGammas(s);
-time X, phi := BelyiMap(Gammas[8] : prec := 50, precNewton := 1000);
+time X, phi := BelyiMap(Gammas[8] : prec := 35, precNewton := 300);
 K<nu> := BaseField(X);
