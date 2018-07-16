4da157e5b8efe917c4e01834b3a79c453d06ecca
Sam Schiavone <sam.schiavone@gmail.com>
2018-07-15 21:40:49 -0500

    changed prec

--- ants.m
+++ ants.m
@@ -3,6 +3,6 @@ load "config.m";
 s := BelyiDBRead("6T15-[5,5,5]-51-51-51-g1.m");
 Gammas := BelyiDBToGammas(s);
 Gamma := Gammas[1];
-prec := 30;
+prec := 40;
 time X,phi := BelyiMap(Gamma : prec := prec, precNewton := 800);
 K<nu> := BaseField(X);
