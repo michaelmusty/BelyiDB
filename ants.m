35b2befdf74486c4479ffac9464286eda166ea6d
Sam Schiavone <sam.schiavone@gmail.com>
2018-07-16 00:00:35 -0500

    iload example

--- ants.m
+++ ants.m
@@ -1,7 +1,10 @@
 load "config.m";
 s := BelyiDBRead("6T15-[5,5,5]-51-51-51-g1.m");
-Gammas := BelyiDBToGammas(s);
-Gamma := Gammas[1];
-prec := 40;
-time X,phi := BelyiMap(Gamma : prec := prec, precNewton := 800);
-K<nu> := BaseField(X);
+s;
+s`BelyiDBGaloisOrbits;
+s`BelyiDBBelyiCurves;
+BaseFieldDataShort(s);
+s`BelyiDBBelyiMaps;
+phi := s`BelyiDBBelyiMaps[1];
+Support(Divisor(phi));
+Support(Divisor(phi-1));
