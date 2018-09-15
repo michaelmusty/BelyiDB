295372ba4e21a9da8e53b20ba5c7b55be9294ef7
John Voight <jvoight@gmail.com>
2018-09-15 18:45:10 -0400

    Handle m in genuszero script

--- run_genuszero.m
+++ run_genuszero.m
@@ -12,9 +12,23 @@ getnext := function(d);
 end function;
 
 sIn := getnext(8);
+sIn;
 Gammas := BelyiDBToGammas(sIn);
 for Gamma in Gammas do
-  X, phi := BelyiMap(Gamma : prec := 30);
+  try 
+    X, phi := BelyiMap(Gamma : prec := 30);
+  catch e
+    if "smaller m" in e`Object then
+      m0 := #PassportRepresentatives(DefiningPermutation(Gamma) : Pointed := true);
+      for m := m0-1 to 1 by -1 do
+        try 
+          X, phi := BelyiMap(Gamma : prec := 30, DegreeBound := m);
+        catch e;
+        end try;
+        if assigned Gamma`TriangleBelyiCurve then assert BelyiMapSanityCheck(Gamma); break; end if;
+      end for;
+    end if;
+  end try;
 end for;
 
 sOut := GammasToBelyiDB(Gammas); BelyiDBWrite(sOut);
