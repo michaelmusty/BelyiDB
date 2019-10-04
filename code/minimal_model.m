1031ebfa379a9b68b59b31eb6f8bda614f4f4128
Sam Schiavone <sam.schiavone@gmail.com>
2019-10-04 14:38:14 -0400

    working on MinimalModel-ing genus 1 and genus 2 Belyi maps defined over a number field with class number 1

---
+++ code/minimal_model.m
@@ -0,0 +1,36 @@
+load "config.m";
+filenames := [];
+for i := 1 to 9 do
+  filenames cat:= BelyiDBFilenames(i);
+end for;
+// pick out only g1 names
+g1_names := [];
+for f in filenames do
+  if f[#f-2] eq "1" then
+    Append(~g1_names,f);
+  end if;
+end for;
+// change to minimal model if possible
+for f in g1_names do
+  s := BelyiDBRead(f);
+  assert Genus(s) eq 1;
+  maps := s`BelyiDBBelyiMaps;
+  curves := s`BelyiDBBelyiCurves;
+  curves_min := [**];
+  maps_min := [**];
+  for i := 1 to #curves do
+    X := curves[i];
+    phi := maps[i];
+    K := BaseRing(X);
+    if ClassNumber(K) eq 1 then
+      printf "%o has class number 1; computing minimal model\n", K; 
+      Xmin, mp := MinimalModel(X);
+      phi_min := Pushforward(mp,phi);
+    else
+      Xmin := X;
+      phi_min := phi;
+    end if;
+    Append(~curves_min, Xmin);
+    Append(~maps_min, phi_min);
+  end for;
+end for;
