89b03061ee2066e710dfa7e431855520a7593186
michaelmusty <michaelmusty@gmail.com>
2019-10-17 17:54:22 -0400

    setseqseqset etsetera

--- code/database_code/LLL.m
+++ code/database_code/LLL.m
@@ -1,6 +1,6 @@
 intrinsic GetSpecializationValues() -> Any
   {}
-  return eval Read("scripts/c4c6toJ.m");
+  return SetToSequence(SequenceToSet(eval Read("scripts/c4c6toJ.m")));
 end intrinsic;
 
 intrinsic AFF(phi::FldFunFracSchElt) -> Any
