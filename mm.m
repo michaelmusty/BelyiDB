6a84314e11c8ef75e451c947cea3786d1b0be036
Michael Musty <michaelmusty@gmail.com>
2020-06-12 11:27:14 -0400

    S3Action on BelyiDB object

--- mm.m
+++ mm.m
@@ -4,7 +4,8 @@ filenames := BelyiDBGetFilenames(6,1);
 objs := BelyiDBGet(6,1 : only_hyperbolic := true);
 s := BelyiDBRead("6T15-[5,5,5]-51-51-51-g1.m");
 attr := "BelyiDBBelyiMaps";
-s``attr;  // print attr before
+l0 := s``attr[1];  // print attr before
 S3Action(Sym(3)!(1,2), s);
-s``attr;  // print attr after
+l1 := s``attr[1];  // print attr after
+l0 eq l1;
 exit;
