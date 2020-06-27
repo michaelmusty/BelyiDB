0a7a49799b360f27247e3898f353dd42e10bb163
Sam Schiavone <sam.schiavone@gmail.com>
2020-06-26 21:22:48 -0400

    added label-converting script

--- code/database_code/lmfdb_upload/passports_to_lmfdb.m
+++ code/database_code/lmfdb_upload/passports_to_lmfdb.m
@@ -12,8 +12,8 @@ intrinsic PermutationToCycleStructure(sigma::GrpPermElt) -> MonStgElt
   return s;
 end intrinsic;
 
-intrinsic PermutationTripleToLabel(sigma::SeqEnum[GrpPermElt]) -> MonStgElt
-  {Given a permutation triple sigma}
+intrinsic PermutationTripleToLMFDBLabel(sigma::SeqEnum[GrpPermElt]) -> MonStgElt
+  {Given a permutation triple sigma, return the LMFDB label of its passport}
   assert #sigma eq 3;
   G := sub< Parent(sigma[1]) | sigma >;
   d, k := TransitiveGroupIdentification(G);
@@ -22,7 +22,7 @@ intrinsic PermutationTripleToLabel(sigma::SeqEnum[GrpPermElt]) -> MonStgElt
     label *:= PermutationToCycleStructure(sig);
     label *:= "_";
   end for;
-  label := label[1..#label-1];
+  label := label[1..#label-1]; // remove last _
   return label;
 end intrinsic;
 
@@ -50,7 +50,61 @@ intrinsic SortPermutations(~sigma::SeqEnum[GrpPermElt])
   sigma := SortPermutations(sigma);
 end intrinsic;
 
-intrinsic BelyiDBObjToLMFDB(s::BelyiDB) -> MonStgElt
-  {}
+// TODO: doesn't work for 2-digit ramification indices
+intrinsic LMFDBLabelToBelyiDBLabel(label::MonStgElt : dot_m := false) -> MonStgElt
+  {Given a LMFDB Belyi passport label, e.g., "7T7-7_4.3_4.3", return the corresponding BelyiDB passports
+    label, e.g., "7T7-[7,12,12]-7-43-43-g2". If dot_m, then include .m in the output.}
+  slabel := Split(label, "-");
+  label_new := "";
+  label_new *:= slabel[1];
+  label_new *:= "-";
+  rams := Split(slabel[2], "_");
+  tail := "";
+  ram_inds := [];
+  for ram in rams do
+    sram := Split(ram, ".");
+    tail *:= Join(sram, "")*"-";
+    Append(~ram_inds, [eval el : el in sram]);
+  end for;
+  tail := tail[1..#tail-1];
+  lcms := [Lcm(el) : el in ram_inds];
+  label_new *:= ReplaceString(Sprint(lcms), " ", "")*"-";
+  label_new *:= tail*"-";
+  // compute genus
+  d := eval Split(slabel[1],"T")[1];
+  degK := -2*d;
+  for inds in ram_inds do
+    for e in inds do
+      degK +:= (e-1);
+    end for;
+  end for;
+  assert degK mod 2 eq 0;
+  label_new *:= "g"*Sprint((degK+2) div 2);
+  if dot_m then
+    label_new *:= ".m";
+  end if;
+  return label_new;
+end intrinsic;
 
+intrinsic ShortGeometryType(s::BelyiDB) -> MonStgElt
+  {}
+  if TriangleType(s) eq "Hyperbolic" then
+    return "H";
+  elif TriangleType(s) eq "Euclidean" then
+    return "E";
+  else
+    assert TriangleType(s) eq "Spherical";
+    return "S";
+  end if;
+end intrinsic;
+
+intrinsic PassportFileHeaders() -> MonStgElt
+  {}
+  return "geomtype|pass_size|abc|group|g|maxdegbf|lambdas|plabel|num_orbits|deg|a_s|b_s|c_s";
+end intrinsic;
+
+intrinsic BelyiDBObjToLMFDB(s::BelyiDB) -> MonStgElt
+  {return string containing one line}
+  s := "";
+  s *:= ShortGeometryType(s);
 end intrinsic;
---
+++ code/database_code/utils.m
@@ -0,0 +1,34 @@
+intrinsic ReplaceString(s::MonStgElt, fs::MonStgElt, ts::MonStgElt) -> MonStgElt
+  {Return a string obtained from the string s by replacing all occurences of fs with ts.}
+  assert fs ne ts;
+  s:=CodeToString(255) cat Sprint(s) cat CodeToString(255);
+  while Position(s,fs) ne 0 do
+    p:=Position(s,fs);
+    p:=[p-1,#fs,#s-p-#fs+1];
+    s1,s2,s3:=Explode(Partition(Eltseq(s),p));
+    s:=&cat(s1) cat ts cat &cat(s3);
+  end while;
+  return s[[2..#s-1]];
+end intrinsic;
+
+intrinsic ReplaceString(s::MonStgElt, fs::[MonStgElt], ts::[MonStgElt]) -> MonStgElt
+  {Return a string obtained from the string s by replacing all occurences of strings in fs with strings in ts.}
+  //assert fs ne ts;
+  for i:=1 to #fs do
+    s:=ReplaceString(s,fs[i],ts[i]);
+  end for;
+  return s;
+end intrinsic;
+
+// procedure versions
+intrinsic ReplaceString(~s::MonStgElt, fs::MonStgElt, ts::MonStgElt)
+  {In the string s, replace all occurences of fs with ts.}
+  s := ReplaceString(s,fs,ts);
+end intrinsic;
+
+intrinsic ReplaceString(~s::MonStgElt, fs::[MonStgElt], ts::[MonStgElt])
+  {In the string s, replace all occurences of strings in fs with strings in ts.}
+  for i:=1 to #fs do
+    ReplaceString(~s,fs[i],ts[i]);
+  end for;
+end intrinsic;
