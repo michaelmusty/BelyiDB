b746d8dbe81dcc1ed19256f9f79746ba6e475fb3
Edgar Costa <edgarc@mit.edu>
2021-07-31 14:54:26 -0400

    the null

--- code/database_code/lmfdb_upload/magma_to_lmfdb.m
+++ code/database_code/lmfdb_upload/magma_to_lmfdb.m
@@ -1,62 +1,3 @@
-// define which attributes can be acted on by S3
-intrinsic GetS3Attrs(s::BelyiDB) -> SeqEnum[MonStgElt]
-  {}
-  l := [];
-  Append(~l, "BelyiDBPermutationTriple");
-  Append(~l, "BelyiDBPassport");
-  Append(~l, "BelyiDBPointedPassport");
-  Append(~l, "BelyiDBGaloisOrbits");
-  Append(~l, "BelyiDBBelyiMaps");
-  return l;
-end intrinsic;
-
-// S3Action on (attributes of) s by tau
-intrinsic S3Action(tau::GrpPermElt, s::BelyiDB) -> BelyiDB
-  {}
-  for attr in GetS3Attrs(s) do
-    s``attr := S3Action(tau, s``attr);
-  end for;
-  return s;
-end intrinsic;
-
-// S3Action for a list of triples
-intrinsic S3Action(tau::GrpPermElt, l::SeqEnum[SeqEnum[GrpPermElt]]) -> SeqEnum[SeqEnum[GrpPermElt]]
-  {}
-  return [S3Action(tau, x) : x in l];
-end intrinsic;
-
-// S3Action for Galois orbits lists of (lists of triples)
-intrinsic S3Action(tau::GrpPermElt, l::SeqEnum[SeqEnum[SeqEnum[GrpPermElt]]]) -> SeqEnum[SeqEnum[SeqEnum[GrpPermElt]]]
-  {}
-  return [S3Action(tau, x) : x in l];
-end intrinsic;
-
-// S3Action for maps
-intrinsic S3Action(tau::GrpPermElt, phi::FldFunFracSchElt) -> FldFunFracSchElt
-  {}
-  S := Sym(3);
-  assert Parent(tau) eq S;
-  if tau eq S!(1,2) then
-    return 1-phi;
-  elif tau eq S!(1,3) then
-    return 1/phi;
-  elif tau eq S!(2,3) then
-    return phi/(1-phi);
-  elif tau eq S!(1,2,3) then
-    return 1/phi-1;
-  elif tau eq S!(1,3,2) then
-    return 1/(1-phi);
-  else
-    return phi;
-  end if;
-end intrinsic;
-
-// S3Action for list of maps
-intrinsic S3Action(tau::GrpPermElt, l::List) -> List
-  {}
-  return [* S3Action(tau, x) : x in l *];
-end intrinsic;
-
 // list of search cols
 galmap_column_handler := [
 // <name, type, function, needs inds and index>
@@ -80,17 +21,17 @@ galmap_column_handler := [
   <"embeddings", "jsonb", Embeddings, true>,
   <"plabel", "test", PassportLabel, false>,
   <"lambdas", "jsonb", LambdaSt, false>,
-  <"friends", >,
-  <"curve_label", >,
+  <"friends", "text[]", NULL, true>,
+  <"curve_label", "text", NULL, true>,
   <"BelyiDB_label", BelyiDB_label, true>,
   <"BelyiDB_plabel", "text", BelyiDB_plabel, false>,
-  <"specializations", >,
-  <"models", >,
-  <"moduli_field", >,
-  <"moduli_field_label", >,
-  <"base_field_label", >,
-  <"primitivization", >,
-  <"is_primitive", >
+  <"specializations", "jsonb", NULL, true>,
+  <"models", "jsonb", NULL, true>,
+  <"moduli_field", "numeric[]", NULL, true>,
+  <"moduli_field_label", "text", NULL, true>,
+  <"base_field_label", "text", NULL, true>,
+  <"primitivization", "text", NULL, true>,
+  <"is_primitive", "bool", NULL, true>
 ];
 
 intrinsic BelyiDBToLMFDB(s::BelyiDB, inds::SeqEnum[RngIntElt], index::RngIntElt) -> MonStgElt
--- code/database_code/lmfdb_upload/utils.m
+++ code/database_code/lmfdb_upload/utils.m
@@ -1,3 +1,13 @@
+intrinsic NULL(s::BelyiDB) -> MonStgElt
+  {}
+  return "NULL";
+end if;
+
+intrinsic NULL(s::BelyiDB, inds::SeqEnum[RngIntElt], index::RngIntElt) -> MonStgElt
+  {}
+  return "NULL";
+end if;
+
 intrinsic BelyiDB_plabel(s::BelyiDB) -> MonStgElt
   {}
   return Name(s);
@@ -422,4 +432,65 @@ intrinsic sprint(X::.) -> MonStgElt
 { Sprints object X with spaces and carriage returns stripped. }
     if Type(X) eq Assoc then return Join(Sort([ $$(k) cat "=" cat $$(X[k]) : k in Keys(X)]),":"); end if;
     return strip(Sprintf("%o",X));
+end intrinsic;
+
+
+//S3 stuff
+// define which attributes can be acted on by S3
+intrinsic GetS3Attrs(s::BelyiDB) -> SeqEnum[MonStgElt]
+  {}
+  l := [];
+  Append(~l, "BelyiDBPermutationTriple");
+  Append(~l, "BelyiDBPassport");
+  Append(~l, "BelyiDBPointedPassport");
+  Append(~l, "BelyiDBGaloisOrbits");
+  Append(~l, "BelyiDBBelyiMaps");
+  return l;
+end intrinsic;
+
+// S3Action on (attributes of) s by tau
+intrinsic S3Action(tau::GrpPermElt, s::BelyiDB) -> BelyiDB
+  {}
+  for attr in GetS3Attrs(s) do
+    s``attr := S3Action(tau, s``attr);
+  end for;
+  return s;
+end intrinsic;
+
+// S3Action for a list of triples
+intrinsic S3Action(tau::GrpPermElt, l::SeqEnum[SeqEnum[GrpPermElt]]) -> SeqEnum[SeqEnum[GrpPermElt]]
+  {}
+  return [S3Action(tau, x) : x in l];
+end intrinsic;
+
+// S3Action for Galois orbits lists of (lists of triples)
+intrinsic S3Action(tau::GrpPermElt, l::SeqEnum[SeqEnum[SeqEnum[GrpPermElt]]]) -> SeqEnum[SeqEnum[SeqEnum[GrpPermElt]]]
+  {}
+  return [S3Action(tau, x) : x in l];
+end intrinsic;
+
+// S3Action for maps
+intrinsic S3Action(tau::GrpPermElt, phi::FldFunFracSchElt) -> FldFunFracSchElt
+  {}
+  S := Sym(3);
+  assert Parent(tau) eq S;
+  if tau eq S!(1,2) then
+    return 1-phi;
+  elif tau eq S!(1,3) then
+    return 1/phi;
+  elif tau eq S!(2,3) then
+    return phi/(1-phi);
+  elif tau eq S!(1,2,3) then
+    return 1/phi-1;
+  elif tau eq S!(1,3,2) then
+    return 1/(1-phi);
+  else
+    return phi;
+  end if;
+end intrinsic;
+
+// S3Action for list of maps
+intrinsic S3Action(tau::GrpPermElt, l::List) -> List
+  {}
+  return [* S3Action(tau, x) : x in l *];
 end intrinsic;
